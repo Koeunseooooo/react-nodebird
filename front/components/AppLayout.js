35fc446229b5533534cc8177b588ce5cd3ad30d5
Koeunseooooo <rhdmstj17@cau.ac.kr>
2023-02-01 10:23:13 +0900

    fix: Menu component warning by antd version upgrade

--- front/components/AppLayout.js
+++ front/components/AppLayout.js
@@ -9,25 +9,29 @@ import styled from 'styled-components'
 const SearchInput = styled(Input.Search)`
     vertical-align: middle
 `
-
+const menuItems = [
+    {
+        label: <Link href="/"><a>노드버드</a></Link>,
+        key: "home",
+    },
+    {
+        label: <Link href="/profile"><a>프로필</a></Link>,
+        key: "profile",
+    },
+    {
+        label: <SearchInput enterButton />,
+        key: "search",
+    },
+    {
+        label: <Link href="/signup"><a>회원가입</a></Link>,
+        key: "signup",
+    },
+]
 const AppLayout = ({children}) => {
     const [isLoggedIn, setIsLoggedIn] = useState(false)
     return (
         <div>
-            <Menu mode="horizontal">
-                <Menu.Item>
-                    <Link href="/"><a>노드버드</a></Link>
-                </Menu.Item>
-                <Menu.Item>
-                    <Link href="/profile"><a>프로필</a></Link>
-                </Menu.Item>
-                <Menu.Item>
-                    <SearchInput enterButton />
-                </Menu.Item>
-                <Menu.Item>
-                    <Link href="/signup"><a>회원가입</a></Link>
-                </Menu.Item>
-            </Menu>
+            <Menu mode="horizontal" items={menuItems}/>
             <Row gutter={8}>
                 <Col xs={24} md={6}>
                     {isLoggedIn ? <UserProfile /> : <LoginForm setIsLoggedIn={setIsLoggedIn} />}
