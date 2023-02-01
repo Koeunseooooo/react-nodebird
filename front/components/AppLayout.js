432264ac3c89d9e01013aeee83a5aaf0bcef0aa2
Koeunseooooo <rhdmstj17@cau.ac.kr>
2023-02-01 10:40:28 +0900

    fix: not defined error when click logout button in UserProfile

--- front/components/AppLayout.js
+++ front/components/AppLayout.js
@@ -27,6 +27,7 @@ const menuItems = [
         key: "signup",
     },
 ]
+
 const AppLayout = ({children}) => {
     const [isLoggedIn, setIsLoggedIn] = useState(false)
     return (
@@ -34,7 +35,7 @@ const AppLayout = ({children}) => {
             <Menu mode="horizontal" items={menuItems}/>
             <Row gutter={8}>
                 <Col xs={24} md={6}>
-                    {isLoggedIn ? <UserProfile /> : <LoginForm setIsLoggedIn={setIsLoggedIn} />}
+                    {isLoggedIn ? <UserProfile setIsLoggedIn={setIsLoggedIn} /> : <LoginForm setIsLoggedIn={setIsLoggedIn} />}
                 </Col>
                 <Col xs={24} md={6}>
                     {children}
